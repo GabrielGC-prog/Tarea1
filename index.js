813d09a76d30d9cfac9c4f31b1bf0e31d51d8fac
GabrielGC-prog <gutierrezcorral@proton.me>
2025-06-29 20:54:43 -0300

    Tarea 1 prueba

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/*
-DOM => document objet mode
-va a ser el documento html
-capturar elementos del dom (html) los voy a trabajar en el archivo JS y los voy a "retornar" al html
-- escuchador de eventos=> los q se hacen con el mouse y los del teclado
-
-- crear elementos html e insertarlos en el documento original
-
-cuando se le de click al boton
-el valor que tenia el input se muestre dentro del div
-con el formato Hola ${nombre}
-*/
-
 const btnEl = document.getElementById("crearCardBtn");
 const contenedorEl = document.getElementById("contedorCards");
 
@@ -23,4 +10,3 @@ btnEl.addEventListener("click", () => {
   `;
   contenedorEl.appendChild(card);
 });
-/* quede en 1 hora 10minutos del video de la clase 8*/
